20956193d90eaf85e5a653b4a521eb392c0c7dd1
ahanasmeegg <1527926614@qq.com>
2025-02-08 01:49:12 +0800

    Typed function signature.

--- src/02-functions.ts
+++ src/02-functions.ts
@@ -1,3 +1,38 @@
 import {Friend, Colleague } from './myTypes'
+import { friends, colleagues } from './01-basics';
 
+function older(f: Friend) : string {
+    f.age += 1
+    return `${f.name} is now ${f.age}` 
+}
 
+console.log(older(friends[0]))
+
+function allOlder(friends: Friend[]): string[] {
+    return friends.map(f => older(f));
+  }
+  
+console.log(allOlder(friends));
+  
+// Find the colleague with the highest extension number.
+function highestExtension(cs: Colleague[]): Colleague {
+    const result = cs.sort(
+      (c1, c2) => c1.contact.extension - c2.contact.extension
+    );
+    return result[cs.length - 1];
+  }
+  console.log(highestExtension(colleagues.current));
+  
+function addColleague(cs: Colleague[], name: string, department: string, email: string) {
+    const maxExtension = cs.length ? Math.max(...cs.map(c => c.contact.extension)) : 100;
+    const newColleague: Colleague = {
+      name,
+      department,
+      contact: { email, extension: maxExtension + 1 }
+    };
+    cs.push(newColleague);
+  }
+  
+  addColleague(colleagues.current, "Sheild O Connell", "HR", "soc@here.com");
+  console.log(colleagues.current.filter(c => c.name === "Sheild O Connell"));
+  
